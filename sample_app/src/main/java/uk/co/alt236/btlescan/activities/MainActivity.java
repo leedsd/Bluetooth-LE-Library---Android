4f9b0acdaec6ff2af014a9f87aaafabe04528b36
leedsd <leedsd99@gmail.com>
2016-07-04 10:27:08 +0900

    Add application filter for BLE scan.
    Add application filter for BLE scan.

--- sample_app/src/main/java/uk/co/alt236/btlescan/activities/MainActivity.java
+++ sample_app/src/main/java/uk/co/alt236/btlescan/activities/MainActivity.java
@@ -44,21 +44,81 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private LeDeviceListAdapter mLeDeviceListAdapter;
     private BluetoothLeDeviceStore mDeviceStore;
 
+    private final static UUID UUID_DELTA =
+            UUID.fromString("9760d077-a234-4686-9e00-fcbbee3373f6");
+
+    // Filtering by custom UUID is broken in Android 4.3 and 4.4, see:
+    //   http://stackoverflow.com/questions/18019161/startlescan-with-128-bit-uuids-doesnt-work-on-native-android-ble-implementation?noredirect=1#comment27879874_18019161
+    // This is a workaround function from the SO thread to manually parse advertisement data.
+    private List<UUID> parseUUIDs(final byte[] advertisedData) {
+        List<UUID> uuids = new ArrayList<UUID>();
+
+        int offset = 0;
+        while (offset < (advertisedData.length - 2)) {
+            int len = advertisedData[offset++];
+            if (len == 0)
+                break;
+
+            int type = advertisedData[offset++];
+            switch (type) {
+                case 0x02: // Partial list of 16-bit UUIDs
+                case 0x03: // Complete list of 16-bit UUIDs
+                    while (len > 1) {
+                        int uuid16 = advertisedData[offset++];
+                        uuid16 += (advertisedData[offset++] << 8);
+                        len -= 2;
+                        uuids.add(UUID.fromString(String.format("%08x-0000-1000-8000-00805f9b34fb", uuid16)));
+                    }
+                    break;
+                case 0x06:// Partial list of 128-bit UUIDs
+                case 0x07:// Complete list of 128-bit UUIDs
+                    // Loop through the advertised 128-bit UUID's.
+                    while (len >= 16) {
+                        try {
+                            // Wrap the advertised bits and order them.
+                            ByteBuffer buffer = ByteBuffer.wrap(advertisedData, offset++, 16).order(ByteOrder.LITTLE_ENDIAN);
+                            long mostSignificantBit = buffer.getLong();
+                            long leastSignificantBit = buffer.getLong();
+                            uuids.add(new UUID(leastSignificantBit,
+                                    mostSignificantBit));
+                        } catch (IndexOutOfBoundsException e) {
+                            // Defensive programming.
+                            //Log.e(LOG_TAG, e.toString());
+                            continue;
+                        } finally {
+                            // Move the offset to read the next uuid.
+                            offset += 15;
+                            len -= 16;
+                        }
+                    }
+                    break;
+                default:
+                    offset += (len - 1);
+                    break;
+            }
+        }
+        return uuids;
+    }
+
     private final BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
 
             final BluetoothLeDevice deviceLe = new BluetoothLeDevice(device, rssi, scanRecord, System.currentTimeMillis());
-            mDeviceStore.addDevice(deviceLe);
-            final EasyObjectCursor<BluetoothLeDevice> c = mDeviceStore.getDeviceCursor();
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mLeDeviceListAdapter.swapCursor(c);
-                    updateItemCount(mLeDeviceListAdapter.getCount());
-                }
-            });
+            // Check if the device has the Delta service.
+            if (parseUUIDs(scanRecord).contains(UUID_DELTA)) {
+                mDeviceStore.addDevice(deviceLe);
+                final EasyObjectCursor<BluetoothLeDevice> c = mDeviceStore.getDeviceCursor();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLeDeviceListAdapter.swapCursor(c);
+                        updateItemCount(mLeDeviceListAdapter.getCount());
+                    }
+                });
+            }
         }
     };
 
